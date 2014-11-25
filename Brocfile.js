ce9f9ea7739e110f9ba676c4d02c87d9c6657a13
Bradley Schaefer <bradley.schaefer@gmail.com>
2014-11-25 10:14:21 -0600

    Remove bootstrap and default styles

--- Brocfile.js
+++ Brocfile.js
@@ -19,13 +19,8 @@ var app = new EmberAddon();
 
 // dependency for the highlight-code component
 app.import('bower_components/highlightjs/highlight.pack.js');
-app.import('bower_components/boxuk/index.css');
 
 // load sinon for test spies
 app.import('bower_components/sinon/index.js');
-
-// this is just to override the default select2 css to look better with bootstrap
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/select2-bootstrap/select2-bootstrap.css');
 
 module.exports = app.toTree();
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = {
     this._super.included(app);
  
     app.import(app.bowerDirectory + '/select2/select2.js');
-    app.import(app.bowerDirectory + '/select2/select2.css');
     app.import(app.bowerDirectory + '/select2/select2.png', { destDir: 'assets' });
     app.import(app.bowerDirectory + '/select2/select2x2.png', { destDir: 'assets' });
     app.import(app.bowerDirectory + '/select2/select2-spinner.gif', { destDir: 'assets' });
